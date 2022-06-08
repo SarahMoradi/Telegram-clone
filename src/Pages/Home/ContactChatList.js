7bd36ece569d1a13836e39028cc0b3801d947cc3
Moradi <s.moradi@asiatech.cloud>
2022-06-08 15:48:03 +0430

    added off-canvas-menu for more options

--- src/Pages/Home/ContactChatList.js
+++ src/Pages/Home/ContactChatList.js
@@ -7,34 +7,77 @@ import { GrFormSearch } from "react-icons/gr";
 import { fetchUsers } from "../../redux/user/UserAction";
 import styles from "./Home.module.css";
 import { useEffect, useState } from "react";
-
+import { OffCanvas, OffCanvasBody, OffCanvasMenu } from "react-offcanvas";
+import { MdOutlineClose } from "react-icons/md";
 const ContactChatList = () => {
   const userData = useSelector((state) => state.users);
   const dispatch = useDispatch();
   const { users, error, loading } = userData;
   const userId = localStorage.getItem("userId");
+  const [state, setState] = useState(false);
+
+  const handleClick = () => {
+    setState(!state);
+  };
+
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
 
   return (
     <Col sm="12" xl="3" className="d-flex flex-column">
-
       <div className={`${styles.nav_bar_section}`}>
         {" "}
         <div
           className={`${styles.header_nav_bar} d-flex align-items-center justify-content-between px-2`}
         >
-          <div
-            className="d-flex align-items-center"
-            onClick={() => {}} >
-            <GiHamburgerMenu
-              color="white"
-              className="mx-2 my-3 cursor-pointer"
-              size={20}
-            />
-            <p className={`${styles.contact_name_logo} my-2`}>Telegram</p>
+          <div className="d-flex align-items-center">
+            {!state ? (
+              <GiHamburgerMenu
+                color="white"
+                className="mx-2 my-3 cursor-pointer"
+                size={20}
+                onClick={handleClick}
+              />
+            ) : (
+              <MdOutlineClose
+                color="white"
+                className="mx-2 my-3 cursor-pointer"
+                size={20}
+                onClick={handleClick}
+              />
+            )}
+            <p className={`${styles.contact_name_logo} my-2`}>
+              {!state ? "Telegram" : "Menu"}
+            </p>
           </div>
+          <OffCanvas
+            width={300}
+            transitionDuration={300}
+            isMenuOpened={state}
+            position={"left"}
+            effect={"overlay"}
+          >
+            <OffCanvasMenu className={`${styles.off_canvas_menu}`}>
+              <img src="/background.jpg" alt="default" />
+              <div className={`d-flex ${styles.off_canvas_header}`}>
+                <img src="/user.png" alt="defaultUser" />
+
+                {users
+                  .filter((user) => user._id === userId)
+                  .map((user) => {
+                    return (
+                      <>
+                        <p>
+                          {user.firstName} {user.lastName}
+                        </p>
+                        <p>+98 {user.phoneNumber}</p>
+                      </>
+                    );
+                  })}
+              </div>
+            </OffCanvasMenu>
+          </OffCanvas>
           <div>
             <GrFormSearch size={32} className={`${styles.search_icon}`} />
           </div>
--- src/Test.js
+++ src/Test.js
@@ -1,7 +1,33 @@
+import {useState} from "react";
+import { OffCanvas, OffCanvasBody, OffCanvasMenu } from "react-offcanvas";  
 const Test = () => {
+    
+  const [state, setState] = useState(false);
+
+  const handleClick = () => {
+    // toggles the menu opened state
+    setState(!state);
+  };
+
   return (
     <>
-      <p>Hello</p>
+      <button onClick={handleClick}>Open</button>
+      <OffCanvas
+        width={300}
+        transitionDuration={300}
+        isMenuOpened={state}
+        position={"left"}
+        effect={"overlay"}
+      >
+        
+        
+        <OffCanvasMenu
+          className={"my-menu-class"}
+          style={{ fontWeight: "bold", marginTop: "20px" }}
+        >
+          This is the canvas menu.
+        </OffCanvasMenu>
+      </OffCanvas>
     </>
   );
 };
