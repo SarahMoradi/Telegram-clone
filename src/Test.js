1da13ea1dbcf1dad50b9acdbf8e3c9bc5622cfa6
Moradi <s.moradi@asiatech.cloud>
2022-06-08 10:42:33 +0430

    fixed bug in rendering routes

---
+++ src/Test.js
@@ -0,0 +1,8 @@
+const Test = () => {
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+};
+export default Test;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Login from "./Authentication/Login/Login";
 import Signup from "./Authentication/Sign-up/Signup";
 import Home from "./Pages/Home/Home";
+import Test from "./Test";
 
 const routes = [
   { path: "/chat", element: <Home /> },
+  { path: "/new", element: <Test /> },
   /////add splash screen in authentication
   { path: "/authentication", element: <></> },
   { path: "/authentication/sign-up", element: <Signup /> },
