7eda2b053241529f986e7f54ecbcde7c635113bd
Sarah Moradi <moradisarah2001@gamil.com>
2022-06-17 19:46:54 +0430

    fixed option handler in list

--- src/Layout/OffCanvas/ListOptions.js
+++ src/Layout/OffCanvas/ListOptions.js
@@ -4,41 +4,37 @@ import { BiLogOutCircle } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
 
 const OptionList = () => {
-    const navigate = useNavigate();
-    const logoutHandler = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userId');
-        navigate('/authentication/login');
-      };
-      const signUpHandler = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userId');
-        navigate('/authentication/sign-up');
-      };
-      const savedMessageHandler = () => {
-        
-      }
-    return ( <><div
-        onClick={signUpHandler}
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    navigate('/authentication/login');
+  };
+  const signUpHandler = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    navigate('/authentication/sign-up');
+  };
+  const savedMessageHandler = () => {
+    console.log('Saved Message');
+  };
+  return (
+    <>
+      <div
+        onClick={savedMessageHandler}
         className='d-flex align-items-center option-list-in-offcanvas mt-3 mx-2'
       >
         <span>
-          <MdSaveAlt
-            size={23}
-            className='icons-in-options-list'
-          />
+          <MdSaveAlt size={23} className='icons-in-options-list' />
           Saved Message
         </span>
       </div>
       <div
-        onClick={savedMessageHandler}
+        onClick={signUpHandler}
         className='d-flex align-items-center option-list-in-offcanvas mt-2 mx-2'
       >
         <span>
-          <MdAssignmentInd
-            size={23}
-            className='icons-in-options-list'
-          />
+          <MdAssignmentInd size={23} className='icons-in-options-list' />
           Sign up
         </span>
       </div>
@@ -47,13 +43,12 @@ const OptionList = () => {
         className='d-flex align-items-center option-list-in-offcanvas mt-2 mx-2'
       >
         <span>
-          <BiLogOutCircle
-            size={23}
-            className='icons-in-options-list'
-          />
+          <BiLogOutCircle size={23} className='icons-in-options-list' />
           Log out
         </span>
-      </div></> );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default OptionList;
